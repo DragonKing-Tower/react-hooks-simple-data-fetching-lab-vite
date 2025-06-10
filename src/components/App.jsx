7c7c100fb5d17e03b886d45ef31ab161454f2c38
Cyril Rafalovich <replanttrees@gmail.com>
2025-06-09 16:56:39 -1000

    Lab all done in App for such simple feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,38 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+	const [image, setImage] = useState("");
+	const [loading, setLoading] = useState(true);
+
+	async function handleClick() {
+		setLoading(true);
+		try {
+			console.log("getting dog");
+			const data = await fetch("https://dog.ceo/api/breeds/image/random");
+			const dog = await data.json();
+			setImage(dog.message);
+		} catch (error) {
+			console.log(`Error: ${error}`);
+		} finally {
+			setLoading(false);
+		}
+	}
+
+	useEffect(() => {
+		handleClick();
+	}, []);
+
+	return (
+		<div>
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<img src={image} alt="random dog"></img>
+			)}
+			<button onClick={handleClick}>New Dog</button>
+		</div>
+	);
+}
+
+export default App;
